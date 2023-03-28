10c2906f449dd289cd3499eb3dad5f100b20f481
Emil Overbeck <emil.a.overbeck@gmail.com>
2023-03-28 21:11:56 +0200

    Document the purpose and license of the software

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
 # hintegram
 
-tools to automatically setup standalone educational computers running gnu/linux
+To be used or modified as the reader pleases.
 
-features tba:
-- optionally downloading larger useful archive files (ted-ed, crash course etc)
-- combine with xubuntu in one iso (for release)
+More information available [here](https://drive.google.com/drive/u/1/folders/1povknwA0eCiID84lqbvH1no2gdcHstoq).
 
-changelog:
-- fixed local installation sequence
-- fixed jackd2 interactive behaviour
-- general polish
+## Purpose
+
+This script is a charitable effort to help children in Ghana obtain tools for a
+better education. It is designed to automatically set up laptops with various
+educational software (Wikipedia database, physics simulator...). We used it to
+set up laptops running outdated copies of Windows, which were later sent to the
+aforementioned children in Ghana.
+
+# License - The Unlicense
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+software, either in source code form or as a compiled binary, for any purpose,
+commercial or non-commercial, and by any means.
+
+In jurisdictions that recognize copyright laws, the author or authors of this
+software dedicate any and all copyright interest in the software to the public
+domain. We make this dedication for the benefit of the public at large and to
+the detriment of our heirs and successors. We intend this dedication to be an
+overt act of relinquishment in perpetuity of all present and future rights to
+this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
--- setup.sh
+++ setup.sh
@@ -1,26 +1,48 @@
 #!/usr/bin/env bash
+#
+# Automatically set up Ubuntu for Hintegram with external binaries and libraries.
+# More information available here: <https://drive.google.com/drive/u/1/folders/1povknwA0eCiID84lqbvH1no2gdcHstoq>
+#
+# Created by Swarthe, with help from kinnounko (Github).
+#
 
-#Fully automatic Ubuntu setup script, intended for Hintegram
-#Requires certain local or remote binaries and archives to function
-#Requires that names of user and home directory are equivalent
+# TODO
+#
+# completely overhaul in almost every way, and with features from
+# <https://github.com/Swarthe/utility>
+#
+# have script print source and invitation to report bugs/recommendations and
+# maybe print authors+copyright(?) as well
+#
+# turn script into two parts (probably different files): one to download and do
+# basic setup of necessary files (bypasses possible licensing issues of
+# packaging together) (we can then share the package physically (maybe also with
+# linux iso?)). second part would deploy files and configure etc
+#
 
-#Exit trap
+#
+# Exit trap
+#
 trap \
 "{
   echo "Exiting..."
-  exit 1; 
+  exit 1;
 }" \
 ERR SIGINT SIGTERM
 
-#Privilege test
+#
+# Test privilege level
+#
 if ! [ $(id -u) = 0 ]; then
   echo "Setup is not running as root!"
   echo "Exiting..."
   exit 1
 fi
 
-#Ouput banners
-banner_large()
+#
+# Ouput banners
+#
+banner_large ()
 {
   echo "+------------------------------------------+"
   printf "| %-40s |\n" "`date`"
@@ -29,34 +51,39 @@ banner_large()
   echo "+------------------------------------------+"
 }
 
-banner_small()
+banner_small ()
 {
   echo "+------------------------------------------+"
   printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
   echo "+------------------------------------------+"
 }
 
-#Local files test for profiles
+#
+# Test local files for profiles
+#
 if [ -e deb/sonic-pi_2.10.0~repack-2.1build2_amd64.deb ] && \
-  [ -e deb/gcc-multilib_4%3a9.3.0-1ubuntu2_amd64.deb ] && \
-  [ -e deb/kiwix_2.0.5~focal_amd64.deb ] && \
-  [ -e deb/scratch_1.4.0.6~dfsg1-6_all.deb ] && \
-  [ -e deb/shotcut_20.02.17-2_amd64.deb ] && \
-  [ -e bin/PhET-Installer_linux.bin ] && \
-  [ -e zim/wikipedia_en_for-schools_2018-09.zim ]; then
-    echo "Essential local files found!"
-    echo "Setup will continue locally"
-    prof="local"
+   [ -e deb/gcc-multilib_4%3a9.3.0-1ubuntu2_amd64.deb ] && \
+   [ -e deb/kiwix_2.0.5~focal_amd64.deb ] && \
+   [ -e deb/scratch_1.4.0.6~dfsg1-6_all.deb ] && \
+   [ -e deb/shotcut_20.02.17-2_amd64.deb ] && \
+   [ -e bin/PhET-Installer_linux.bin ] && \
+   [ -e zim/wikipedia_en_for-schools_2018-09.zim ]; then
+  echo "Essential local files found!"
+  echo "Setup will continue locally"
+  prof="local"
 else
   echo "Essential local files missing!"
   echo "Setup will continue remotely"
   prof="remote"
 fi
 
-#Internet connection test for remote
-if [ "$prof" == "remote" ]; then
+#
+# Test internet connection for remote
+#
+if [ "$prof" = "remote" ]; then
   echo "Seeking internet connection..."
-  if ping -c 1 gnu.org > /dev/null 2>&1; then
+
+  if ping -c 1 archlinux.org > /dev/null 2>&1; then
     echo "Internet connection found!"
   else
     echo "No internet connection found!"
@@ -65,23 +92,31 @@ if [ "$prof" == "remote" ]; then
   fi
 fi
 
-#Software install (deb)
+#
+# Install software (deb)
+#
 banner_large "Starting setup..."
 banner_small "Installing software..."
 
 export DEBIAN_FRONTEND=noninteractive
 
-if [ "$prof" == "local" ]; then
+if [ "$prof" = "local" ]; then
   apt -yq install ./deb/*
 fi
 
-if [ "$prof" == "remote" ]; then
-  add-apt-repository -y ppa:kiwixteam/release
-  apt-get -yq install kiwix shotcut sonic-pi scratch gcc-multilib
+if [ "$prof" = "remote" ]; then
+    # Install zoom
+    wget https://zoom.us/client/latest/zoom_amd64.deb -P /tmp/
+    apt -yq install /tmp/zoom_amd64.deb
+
+    add-apt-repository -y ppa:kiwixteam/release
+    apt -yq install kiwix shotcut sonic-pi scratch gcc-multilib
 fi
 
-#Software install (bin)
-if [ "$prof" == "local" ]; then
+#
+# Install software (bin)
+#
+if [ "$prof" = "local" ]; then
   {
     printf "%0.s\n" {1..32}
     echo "/opt/PhET"
@@ -89,15 +124,15 @@ if [ "$prof" == "local" ]; then
     echo "n"
   } | sort | ./bin/PhET-Installer_linux.bin
   cp -v /opt/PhET/PhET\ Simulations.desktop \
-    /usr/share/applications/PhET\ Simulations.desktop
+  /usr/share/applications/PhET\ Simulations.desktop
 fi
 
-if [ "$prof" == "remote" ]; then
+if [ "$prof" = "remote" ]; then
   wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget \
-    --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
-    'https://docs.google.com/uc?export=download&id=1VddMR5dd7BIVp1Ze0PEd5gsU7th0pnTt' -O- | \
-    sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VddMR5dd7BIVp1Ze0PEd5gsU7th0pnTt" \
-    -O /tmp/PhET-Installer_linux.bin
+  --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
+  'https://docs.google.com/uc?export=download&id=1VddMR5dd7BIVp1Ze0PEd5gsU7th0pnTt' -O- | \
+  sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VddMR5dd7BIVp1Ze0PEd5gsU7th0pnTt" \
+  -O /tmp/PhET-Installer_linux.bin
   chmod +x /tmp/PhET-Installer_linux.bin
   {
     printf "%0.s\n" {1..32}
@@ -109,30 +144,43 @@ if [ "$prof" == "remote" ]; then
     /usr/share/applications/PhET\ Simulations.desktop
 fi
 
-#Library download (zim)
+#
+# Download libraries (zim)
+#
 banner_small "Downloading libraries..."
 
-user=$(ls /home/)
+user="$SUDO_USER"
 sudo -u "$user" mkdir -p "/home/"$user"/.local/share/kiwix/"
 
-if [ "$prof" == "local" ]; then
+if [ "$prof" = "local" ]; then
   cp -v "zim/wikipedia_en_for-schools_2018-09.zim" \
   "/home/"$user"/.local/share/kiwix/wikipedia_en_for-schools_2018-09.zim"
   cp -v "zim/library.xml" \
   "/home/"$user"/.local/share/kiwix/library.xml"
 fi
 
-if [ "$prof" == "remote" ]; then
+if [ "$prof" = "remote" ]; then
   sudo -u "$user" wget -O /home/"$user"/.local/share/kiwix/wikipedia_en_for-schools_2018-09.zim \
   https://download.kiwix.org/zim/wikipedia_en_for-schools.zim
   sudo -u "$user" wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget \
-    --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
-    'https://docs.google.com/uc?export=download&id=1JyNEoTIDFXwqRV9rByPsNbry20mCZHRl' -O- | \
-    sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JyNEoTIDFXwqRV9rByPsNbry20mCZHRl" \
-    -O /home/"$user"/.local/share/kiwix/library.xml
+  --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
+  'https://docs.google.com/uc?export=download&id=1JyNEoTIDFXwqRV9rByPsNbry20mCZHRl' -O- | \
+  sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JyNEoTIDFXwqRV9rByPsNbry20mCZHRl" \
+  -O /home/"$user"/.local/share/kiwix/library.xml
 fi
 
-#Automatic reboot
+#
+# Copy shorcuts ; needs kiwix shortcut
+#
+banner_small "Setting up shortcuts..."
+cp -at "/home/$user/Desktop/" /usr/share/applications/org.shotcut.Shotcut.desktop \
+/usr/share/applications/sonic-pi.desktop \
+/usr/share/applications/scratch.desktop \
+/usr/share/applications/PhET\ Simulations.desktop
+
+#
+# Reboot
+#
 banner_large "Setup complete!"
 echo "Rebooting in 7 seconds... Press Ctrl^C to cancel"
 sleep 7
